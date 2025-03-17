2a828d6cda74e83cd88a8b6c21d8d58d92fe65ba
virellajose936 <virellajose936@gmail.com>
2025-03-17 06:02:14 +0000

    Add 10.js

---
+++ src/10.js
@@ -0,0 +1,12 @@
+function randomNodeJSCode(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+// Example usage:
+const code = randomNodeJSCode(10);
+console.log(code);
