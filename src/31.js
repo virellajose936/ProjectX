a942bc1060d459ab08312ee0ad71fd50f8ed4a6e
virellajose936 <virellajose936@gmail.com>
2025-04-26 08:46:45 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,6 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+console.log(getRandomInt(10)); // Output could be 3 or 7 (random numbers between 0 and 9)
